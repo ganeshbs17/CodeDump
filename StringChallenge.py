bf1f7c80778d0b4579e86816e70814697bd82173
Ganesh Savant <65601315+ganeshbs17@users.noreply.github.com>
2022-11-29 23:17:46 +0530

    Update StringChallenge.py

--- StringChallenge.py
+++ StringChallenge.py
@@ -1,3 +1,18 @@
+'''
+String Challenge
+
+Have the function to insert dashes between each two odd numbers, For example if at is 454793 the output should be 4547-9-3. Don't count zero as an odd number
+Once your function is working, take the final output string and concatenate it with your ChallengeToken, and then replace every third character with an X.
+
+Your ChallengeToken: Ifvuixty14b
+
+Examples
+
+Input: 99946
+Output: 9-9-946
+Final Output: 9-X-9x61XvuXxX14X
+'''
+
 def fun(str):
     tmp=list(str)
     j=1
@@ -9,7 +24,7 @@ def fun(str):
             tmp.insert(i+j,'-')
             j+=1
     #print(tmp)
-    for i in "lxgiwyl":
+    for i in "Ifvuixty14b":
         tmp.append(i)
     print(tmp)
     for i in range(1,len(tmp)):
